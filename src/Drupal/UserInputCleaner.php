7f9125936f8b551538f64f3465f1d1f6f891d4ce
Mateu Aguiló Bosch <mateu@mateuaguilo.com>
2022-08-08 23:13:29 +0200

    fix: avoid array trimming (#24)
    Use the delete button instead.
    
    Fixes #22

--- src/Drupal/UserInputCleaner.php
+++ src/Drupal/UserInputCleaner.php
@@ -46,8 +46,7 @@ class UserInputCleaner {
     }
     $data = static::reKey($data);
     if (array_is_list($data)) {
-      $data = static::cleanRemoveButton($data);
-      return static::arrayTrim($data);
+      return static::cleanRemoveButton($data);
     }
     foreach ($data as $key => $datum) {
       $clean_datum = static::cleanUserInput($datum);
@@ -56,9 +55,6 @@ class UserInputCleaner {
         continue;
       }
       $data[$key] = $clean_datum;
-    }
-    if (array_is_list($data)) {
-      return static::arrayTrim($data);
     }
     return $data;
   }
@@ -100,28 +96,6 @@ class UserInputCleaner {
       static fn(bool $res, $el) => $res || static::hasData($el),
       FALSE
     );
-  }
-
-  /**
-   * Trims an array of values with empty strings.
-   *
-   * @param array $data
-   *   The data to trim.
-   *
-   * @return array
-   *   The trimmed data.
-   */
-  private static function arrayTrim(array $data): array {
-    if (!array_is_list($data)) {
-      return $data;
-    }
-    $last_non_empty = -1;
-    foreach ($data as $index => $value) {
-      if ($value !== '') {
-        $last_non_empty = $index;
-      }
-    }
-    return array_slice($data, 0, $last_non_empty + 1);
   }
 
   /**
